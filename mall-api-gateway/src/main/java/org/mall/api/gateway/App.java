31410fea5fdfc2141ef69f912027bff9104b9253
zhangzhifeng <342506754@qq.com>
2019-10-16 16:15:32 +0800

    网关注册APP.java

--- mall-api-gateway/src/main/java/org/mall/api/gateway/App.java
+++ mall-api-gateway/src/main/java/org/mall/api/gateway/App.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * Hello world!
+ * Zuul网关注册
  *
  */
 @SpringBootApplication
